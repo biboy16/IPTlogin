56f786cbeba61ccef989ce4a2785392118bc0967
biboy16 <147641154+biboy16@users.noreply.github.com>
2023-11-11 20:28:42 +0800

    Create Signup.java

---
+++ Signup.java
@@ -0,0 +1,23 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+    public void exitToLogin(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
+    }
+}
